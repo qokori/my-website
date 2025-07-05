6c84fecdf5e01d3d774fdbd2f548f0e9f3aba8da
qokori <equda.979@gmail.com>
2025-07-05 15:30:51 +0500

    some tests

--- scripts.js
+++ scripts.js
@@ -1,18 +1,3 @@
-const buttonA = document.querySelector("#button_A");
-const contentA = document.querySelector("#content_A");
+import gsap from "gsap";
 
-let click = 0;
-
-buttonA.onclick = () => {
-  buttonA.textContent = "Нажми еще!";
-  contentA.textContent = `${click} нажатий на кнопку`;
-  if (click < 10) {
-    click += 1;
-  } else if (click < 20) {
-    buttonA.textContent = "хватит.";
-    click += 1;
-  } else {
-    buttonA.textContent = "хватит.";
-    contentA.textContent = "я сказал хватит.";
-  }
-};
+gsap.to(".selector", { x: 500 });
